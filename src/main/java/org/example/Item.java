15ecb7a2c79ccacc72243b45f4d05a38e53f4896
Brian Michael <brian@cajunbug.net>
2024-12-17 08:12:47 -0500

    Initial checkin

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.DiffBuilder;
+import org.apache.commons.lang3.builder.DiffResult;
+import org.apache.commons.lang3.builder.Diffable;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@Data
+public class Item implements Diffable<Item> {
+
+    private String name;
+
+    private String value;
+
+    @Override
+    public DiffResult<Item> diff(final Item other) {
+        return new DiffBuilder.Builder<Item>()
+                .setLeft(this)
+                .setRight(other)
+                .setStyle(ToStringStyle.JSON_STYLE)
+                .setToStringFormat("Original: %s\n New: %s")
+                .build()
+                .append("name", getName(), other.getName())
+                .append("value", getValue(), other.getValue())
+                .build();
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.apache.commons.lang3.builder.DiffResult;
+
+public class Main {
+    public static void main(String[] args) {
+        final Item item1 = new Item();
+        item1.setName("Name");
+        item1.setValue("Brian");
+
+        final Item item2 = new Item();
+        item2.setName("Name");
+        item2.setValue("Ben");
+
+        final Item item3 = new Item();
+        item3.setName("name");
+        item3.setValue("Ben");
+
+        final Item item4 = new Item();
+        item4.setName("Name");
+        item4.setValue(null);
+
+        DiffResult<Item> diff1 = item1.diff(item1);
+        System.out.println("Same: num of diffs = " + diff1.getNumberOfDiffs() + "\n" + diff1);
+        DiffResult<Item> diff2 = item2.diff(item3);
+        System.out.println("Name only: num of diffs = " + diff2.getNumberOfDiffs() + "\n" + diff2 + "\n");
+        DiffResult<Item> diff3 = item1.diff(item2);
+        System.out.println("Value only: num of diffs = " + diff3.getNumberOfDiffs() + "\n" + diff3 + "\n");
+        DiffResult<Item> diff4 = item1.diff(item3);
+        System.out.println("All: num of diffs = " +  + diff4.getNumberOfDiffs() + "\n" + diff4 + "\n");
+        DiffResult<Item> diff5 = item1.diff(item4);
+        System.out.println("Null: num of diffs = " +  + diff5.getNumberOfDiffs() + "\n" + diff5 + "\n");
+        DiffResult<Item> diff6 = item4.diff(item1);
+        System.out.println("Null (inverse): num of diffs = " +  + diff6.getNumberOfDiffs() + "\n" + diff6 + "\n");
+    }
+}
